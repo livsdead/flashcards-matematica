176694b9b749eaee5a03e553a2089dd4524f2649
livsdead <livia.schebeuka@escola.pr.gov.br>
2024-10-01 07:55:21 -0300

    Add files via upload

---
+++ Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,46 @@
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é inteligência artificial?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é Python?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
